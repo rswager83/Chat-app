332bf5d0d0cdd98872641fd60ce22572dddff558
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-08-11 19:55:59 +0200

    Updating ChatScreen
    Currently working on adding firebase database to app.
    --- Revision needed---

--- components/Chat.js
+++ components/Chat.js
@@ -1,20 +1,62 @@
 import React from "react";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
-import {
-  View,
-  Text,
-  Button,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
+import { View, KeyboardAvoidingView, Platform } from "react-native";
+
+// Firebase Database
+const firebase = require("firebase");
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVwLBVlkHifrF-Nb3eFZpy9B0vfBQPbtQ",
+  authDomain: "test-8cc2f.firebaseapp.com",
+  projectId: "test-8cc2f",
+  storageBucket: "test-8cc2f.appspot.com",
+  messagingSenderId: "93752051851",
+  appId: "1:93752051851:web:b28b0a7a29dcddd3f92c8e",
+  measurementId: "G-6K43FKZQP6",
+};
 
 export default class ChatScreen extends React.Component {
   constructor() {
     super();
     this.state = {
       messages: [],
+      uid: 0,
+      user: {
+        _id: "",
+        name: "",
+        avatar: "",
+      },
     };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    // reference to messages collection
+    this.referenceChatMessages = firebase.firestore().collection("messages");
   }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each doc
+    querySnapshot.forEach((doc) => {
+      // get the queryDocumentSnapshots data
+      var data = doc.data();
+      messages.push({
+        _id: data._id,
+        createdAt: data.createdAt.toDate(),
+        text: data.text,
+        uid: data.uid,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
 
   componentDidMount() {
     // Required for listing name in default message
@@ -22,34 +64,47 @@ export default class ChatScreen extends React.Component {
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
 
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: "Hello developer",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "React Native",
-            avatar: "https://placeimg.com/140/140/any",
-          },
+    // reference to messages collection
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+    // authentication listener
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+
+      // update user state
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        loggedInText: "You are logged in",
+        user: {
+          _id: user._id,
+          name: name,
+          avatar: "https://placeholder.com/140/140/any",
         },
-        // Displays default message and date in chat
-        {
-          _id: 2,
-          text: `${name} has entered the chat.`,
-          createdAt: new Date(),
-          system: true,
-        },
-      ],
+      });
+      // reference to active messages collection
+      this.referenceMessagesUser = firebase
+        .firestore()
+        .collection("messages")
+        .where("uid", "==", this.state.uid);
+
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
     });
   }
 
   // Add messages to state
   onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
   }
 
   // Change properties of the bubble
@@ -62,9 +117,26 @@ export default class ChatScreen extends React.Component {
     );
   }
 
+  // adds messages to store
+  addMessage = (message) => {
+    this.referenceChatMessages.add({
+      uid: this.state.uid,
+      _id: message._id,
+      createdAt: message.createdAt,
+      text: message.text,
+      user: message.user,
+    });
+  };
+
+  componentWillUnmount() {
+    // unsubscribe() used to stop receiving updates from collection
+    this.unsubscribe();
+    this.authUnsubscribe();
+  }
+
   render() {
     // Sets selected background color from start page
-    const { bgColor } = this.props.route.params;
+    let { bgColor, name } = this.props.route.params;
 
     return (
       <View
@@ -78,7 +150,9 @@ export default class ChatScreen extends React.Component {
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
           user={{
-            _id: 1,
+            _id: this.state.user._id,
+            name: name,
+            avatar: this.state.user.avatar,
           }}
         />
         {Platform.OS === "android" ? (
