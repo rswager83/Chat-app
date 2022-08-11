55f8555000ca229f710476e62b46cd4327eeb7e5
rswager83 <101044506+rswager83@users.noreply.github.com>
2022-08-11 11:04:49 +0200

    Update Chat.js
    Adding giftedchat, onSend(), and renderBubble(). Also updating its state.

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,68 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import {
+  View,
+  Text,
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export default class ChatScreen extends React.Component {
-  render() {
-    // Username is displayed
-    let name = this.props.route.params.name;
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    // Required for listing name in default message
+    // Used to display title/name at very top of page
+    let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
 
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        // Displays default message and date in chat
+        {
+          _id: 2,
+          text: `${name} has entered the chat.`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  // Add messages to state
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  // Change properties of the bubble
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{ right: { backgroundColor: "#000" } }}
+      />
+    );
+  }
+
+  render() {
     // Sets selected background color from start page
     const { bgColor } = this.props.route.params;
 
@@ -14,16 +70,20 @@ export default class ChatScreen extends React.Component {
       <View
         style={{
           flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
           backgroundColor: bgColor,
         }}
       >
-        <Text>Chat Screen</Text>
-        <Button
-          title="Go to start"
-          onPress={() => this.props.navigation.navigate("StartScreen")}
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
         />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
